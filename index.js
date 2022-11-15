160b63c31ea33a5b08fdb4c979e0c5032b578169
Majharul-Munna <majharulmunna21@gmail.com>
2022-11-15 17:56:12 +0600

    problrm solved

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+
 
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jed6nqg.mongodb.net/?retryWrites=true&w=majority`;
 
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
         const serviceCollection = client.db('review').collection('services');
         const reviewCollection = client.db ('review').collection('reviews');
-
+        // const result= await serviceCollection.find().toArray();
+        // console.log(result);
         app.get('/services', async(req, res) =>{
             const query ={}
             const cursor = serviceCollection.find(query);
@@ -51,7 +54,7 @@ async function run(){
             const reviews = await cursor.toArray();
             res.send(reviews);
         });
-        app.get('/alllreviews:/id', async(req, res) =>{
+        app.get('/alllreviews/:id', async(req, res) =>{
         
             const id =req.params.id;
             const query={_id:ObjectId(id)}
@@ -75,6 +78,7 @@ async function run(){
         })
     }
     finally{
+        console.log('inside finaly');
 
     }
 }
